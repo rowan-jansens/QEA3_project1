0405ef06b206ea46a8befe2e3c029e11e869e9a7
william-skelly <wskelly@olin.edu>
2022-10-07 10:28:14 -0400

    Initial Commit?

--- matrix_stokes.m
+++ matrix_stokes.m
@@ -15,7 +15,7 @@ vel_feild = inlet_vel .* ones(mesh_height, mesh_length);
 inlet_vector = inlet_vel .* ones(mesh_height, 1);
 zero_row     = zeros(1, mesh_length);
 
-for i = 1:2
+for i = 1:1
     %update matrices:
     bottom_row = vel_feild(mesh_height, :);
     vel_left = [inlet_vector, vel_feild];
@@ -24,8 +24,8 @@ for i = 1:2
     above_diff = above_diff(2:(mesh_height+1),:) - vel_feild;
     below_diff = [zero_row; vel_feild];
     below_diff = below_diff(1:mesh_height,:) - vel_feild;
-    in_delta = viscosity .* (above_diff) ./ cell_height;
-    out_delta = viscosity .* (below_diff) ./ cell_height;
+    in_delta = viscosity .* (above_diff) ./ cell_height
+    out_delta = viscosity .* (below_diff) ./ cell_height
 
     %update velocity feild:
     vel_feild = vel_left + in_delta + out_delta
