6921c2453793cec22451cb39ff7c7af0c72a0a8e
Rowan Jansens <rowan.jansens@gmail.com>
2022-10-13 13:34:21 -0400

    added new code

--- make_animated_plot.m
+++ make_animated_plot.m
@@ -7,30 +7,30 @@ set(gcf, "position", [0 0 600 600])
 
 
 
-subplot(2,1,1)
+% subplot(2,1,1)
 hold on
 [xx, yy] = meshgrid(linspace(0, width, num_elements_x), linspace(0, height, num_elements_y));
 [~, s] = contourf(xx', yy', reshape(temp_tensor(1,:,:), num_elements_x, num_elements_y), 150, 'edgecolor', 'none');
 colorbar;
 colormap hot;
-clim([T_cold T_hot])
+clim([T_0 T_hot])
 axis equal;
 xlabel("X-position (m)", 'Interpreter','Latex')
 ylabel("Y-position (m)", 'Interpreter','Latex')
 scatter(width/2, height/2, 10)
 
 
-subplot(2,1,2)
-p = plot(0, reshape(temp_tensor(1,num_elements_x/2, num_elements_y/2), 1, 1));
-xlim([0 t(end)*1000])
-ylim([T_cold T_hot+100])
-xlabel("Time (ms)", 'Interpreter','Latex')
-ylabel("Temperature at Probe(K)", 'Interpreter','Latex')
+% subplot(2,1,2)
+% p = plot(0, reshape(temp_tensor(1,num_elements_x/2, num_elements_y/2), 1, 1));
+% xlim([0 t(end)*1000])
+% ylim([T_0 T_hot+100])
+% xlabel("Time (ms)", 'Interpreter','Latex')
+% ylabel("Temperature at Probe(K)", 'Interpreter','Latex')
 
 for k=1:length(t)
     s.ZData = reshape(temp_tensor(k,:,:), num_elements_x, num_elements_y);
-    p.XData = t(1:k) .* 1000;
-    p.YData = reshape(temp_tensor(([1:k]),num_elements_x/2, num_elements_y/2), k, 1);
+%     p.XData = t(1:k) .* 1000;
+%     p.YData = reshape(temp_tensor(([1:k]),num_elements_x/2, num_elements_y/2), k, 1);
     sgtitle(sprintf('Temperature\nTime: %.2f ms', t(k) * 1000),...
     'Interpreter','Latex');
     drawnow
