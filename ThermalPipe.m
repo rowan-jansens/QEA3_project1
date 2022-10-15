e1e457ada9033168a782cdb79a946b7992ff62ef
Rowan Jansens <rowan.jansens@gmail.com>
2022-10-15 17:04:22 -0400

    added breading

--- ThermalPipe.m
+++ ThermalPipe.m
@@ -1,20 +1,24 @@
-t_span = [0:0.0001:0.01]; % t is in seconds, give it 25 days
-
-global num_elements_x num_elements_y height width T_hot T_cold; 
-
-width = 0.1;
-height = 0.025;
-num_elements_y = 32;  %must be an even number
-num_elements_x = int16(num_elements_y*(width/height));
+clear
+clc
 
 
-T_0 = 293;
+t_span = [0:1:1000]; % t is in seconds, give it 25 days
+
+global num_elements_x num_elements_y height width T_hot T_cold dx dy k_matrix; 
+
+width = 10;
+height = 2.5;
+num_elements_y = 10;  %must be an even number
+num_elements_x = (width/height) * num_elements_y;
+
+
+T_0 = 273;
 T_hot = 450;
 T_cold = 325;
 
 dTdt = zeros(num_elements_x, num_elements_y);
-
 temps_init = ones(num_elements_x, num_elements_y).*T_0;
+
 temps_init(:,1) = T_hot;
 temps_init(1,:) = T_hot;
 temps_init(:,end) = T_hot;
@@ -23,18 +27,54 @@ temps_init(end,:) = T_hot;
 temps_init = temps_init(:);
 
 
-[t, temps] = ode45(@simulate_temp, t_span, temps_init);
+dx = width/num_elements_x;
+dy = height/num_elements_y;
+
+
+rho = 1100;
+t_cond = 38.4;
+c_v = 3150;
+rho = rho * (dx * dy * .01);
+k_cheese = t_cond / rho / c_v;
+
+
+%testing
+k_cheese = 0.001;
+
+k_bread = k_cheese * 10;
+
+bread_thikness = int16(0.3 * num_elements_y)
+k_matrix = ones(num_elements_x, num_elements_y).*k_cheese;
+k_matrix(:,1:bread_thikness) = k_bread;
+k_matrix(1:bread_thikness,:) = k_bread;
+
+k_matrix(:,end-bread_thikness+1:end) = k_bread;
+k_matrix(end-bread_thikness+1:end,:) = k_bread;
+
+% imagesc(k_matrix)
+% axis equal
+
+
+
+
+options = odeset('Events',@EventsFcn);
+[t, temp_tensor, te,ye,ie] = ode113(@simulate_temp, t_span, temps_init, options);
+
+figure()
+clf
+test = reshape(temp_tensor, length(t), num_elements_x, num_elements_y);
+test = reshape(test(:,num_elements_x/2,num_elements_y/2), 1, length(t));
+plot(t, test)
 
 
 
 
 function res = simulate_temp (~, temps)
-global num_elements_x num_elements_y height width T_hot T_cold; 
-k = 45;
+global num_elements_x num_elements_y dx dy k_matrix; 
+
+
 
 temp_frame = reshape(temps, num_elements_x, num_elements_y);
-dx = width/num_elements_x;
-dy = height/num_elements_y;
 
 
 dTdt(1,:) = 0;
@@ -44,11 +84,21 @@ dTdt(num_elements_x,:) = 0;
 
     for i = 2:num_elements_x-1
         for j = 2:num_elements_y-1
-            dTdt(i,j) = k*(((temp_frame(i+1,j)-2*temp_frame(i,j)+temp_frame(i-1,j))/dx^2) + ...
+            dTdt(i,j) = k_matrix(i,j)*(((temp_frame(i+1,j)-2*temp_frame(i,j)+temp_frame(i-1,j))/dx^2) + ...
                            ((temp_frame(i,j+1)-2*temp_frame(i,j)+temp_frame(i,j-1))/dy^2));
         end
     end
 
-% disp(size(dTdt))
 res = dTdt(:);
+end
+
+
+function [temp_end,isterminal,direction] = EventsFcn(t,temps)
+global num_elements_x num_elements_y T_cold T_hot; 
+temp_frame = reshape(temps, num_elements_x, num_elements_y);
+
+
+  temp_end = temp_frame(num_elements_x/2, num_elements_y/2) - T_hot; % The value that we want to be zero
+  isterminal = 0;  % Halt integration 
+  direction = +1;   % The zero can be approached from either direction
 end
--- make_animated_plot.m
+++ make_animated_plot.m
@@ -1,4 +1,7 @@
-temp_tensor = reshape(temps, length(t), num_elements_x, num_elements_y);
+clc
+temp_tensor = reshape(temp_tensor, length(t), num_elements_x, num_elements_y);
+
+
 
 filename = 'animation.gif';
 figure(1);
@@ -7,7 +10,7 @@ set(gcf, "position", [0 0 600 600])
 
 
 
-% subplot(2,1,1)
+subplot(2,1,1)
 hold on
 [xx, yy] = meshgrid(linspace(0, width, num_elements_x), linspace(0, height, num_elements_y));
 [~, s] = contourf(xx', yy', reshape(temp_tensor(1,:,:), num_elements_x, num_elements_y), 150, 'edgecolor', 'none');
@@ -15,23 +18,47 @@ colorbar;
 colormap hot;
 clim([T_0 T_hot])
 axis equal;
-xlabel("X-position (m)", 'Interpreter','Latex')
-ylabel("Y-position (m)", 'Interpreter','Latex')
+xlabel("X-position (cm)", 'Interpreter','Latex')
+ylabel("Y-position (cm)", 'Interpreter','Latex')
 scatter(width/2, height/2, 10)
+scatter(double(bread_thikness)*dx, height/2, 10)
 
 
-% subplot(2,1,2)
-% p = plot(0, reshape(temp_tensor(1,num_elements_x/2, num_elements_y/2), 1, 1));
-% xlim([0 t(end)*1000])
-% ylim([T_0 T_hot+100])
-% xlabel("Time (ms)", 'Interpreter','Latex')
-% ylabel("Temperature at Probe(K)", 'Interpreter','Latex')
 
-for k=1:length(t)
+
+rectangle('Position',[(double(bread_thikness)*dx) (double(bread_thikness)*dy) (width - 2*double(bread_thikness)*dx) (height - (2*double(bread_thikness)*dy))])
+
+rectangle('Position',[0 0 width height])
+
+
+subplot(2,1,2)
+hold on
+p = plot(0, reshape(temp_tensor(1,num_elements_x/2, num_elements_y/2), 1, 1));
+q = plot(0, reshape(temp_tensor(1,num_elements_x/2, num_elements_y/2), 1, 1));
+grid on
+grid minor
+xlim([0 t(end)])
+ylim([T_0 T_hot])
+legend("Cheese", "Crust")
+xlabel("Time (s)", 'Interpreter','Latex')
+ylabel("Temperature at Probe(K)", 'Interpreter','Latex')
+
+dt = int16(length(t)/40);
+
+for k=1:dt:length(t)
     s.ZData = reshape(temp_tensor(k,:,:), num_elements_x, num_elements_y);
-%     p.XData = t(1:k) .* 1000;
-%     p.YData = reshape(temp_tensor(([1:k]),num_elements_x/2, num_elements_y/2), k, 1);
-    sgtitle(sprintf('Temperature\nTime: %.2f ms', t(k) * 1000),...
+
+
+
+    
+    p.YData = [reshape(temp_tensor(([1:k]),num_elements_x/2, num_elements_y/2), k, 1)];
+    q.YData = [reshape(temp_tensor(([1:k]),bread_thikness/2, num_elements_y/2), k, 1)];
+
+
+p.XData = t(1:k);
+    q.XData = t(1:k);
+
+    sgtitle(sprintf('Temperature\nTime: %.1f s', t(k)),...
     'Interpreter','Latex');
     drawnow
     pause(0.01)
